6e4578234a4f1878b700315d4a829d77d9ccff65
Roy Bailey <roybaileybiz@gmail.com>
2016-04-02 13:00:16 +0100

    tape testing

--- es6/spread-n-rest.js
+++ es6/spread-n-rest.js
@@ -1,26 +1,45 @@
 'use strict';
+import test from 'tape'
+
 
 // spread will spray an array into arguments
+test('ES6 Spread feature', (assert) => {
 
-let oneToFive = [1,2,3,4,5];
-let sixToTen = [6,7,8,9,10];
-let oneToTen = [...oneToFive,...sixToTen];
+    let oneToFive = [1, 2, 3, 4, 5];
+    let sixToTen = [6, 7, 8, 9, 10];
+    let oneToTen = [...oneToFive, ...sixToTen];
 
-// [1,2,3,4,5,6,7,8,9,10]
-console.log(oneToTen);
+    console.log(oneToTen);
+    assert.deepEqual(oneToTen, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
-// rest parameters collect arguments into array...
+    assert.end();
+});
 
-let add = function(...numbers) {
-    return numbers.reduce((a,b)=>a+b);
-};
 
-// 15
-console.log(add(1,2,3,4,5));
+test('ES6 Rest Parameters feature', (assert) => {
 
-let multiplier = function(multiplier,...numbers) {
-    return numbers.map(n=>n*multiplier);
-};
+    // rest parameters collect arguments into array...
+    let add = function (...numbers) {
+        return numbers.reduce((a, b)=>a + b);
+    };
 
-// [10,20,30,40,50]
-console.log(multiplier(10,1,2,3,4,5));
+    let actual = add(1, 2, 3, 4, 5);
+    console.log(actual);
+    assert.equal(actual, 15);
+
+    assert.end();
+});
+
+
+test('ES6 Rest Parameters feature', (assert) => {
+
+    let multiplier = function (multiplier, ...numbers) {
+        return numbers.map(n=>n * multiplier);
+    };
+
+    let actual = multiplier(10, 1, 2, 3, 4, 5);
+    console.log(actual);
+    assert.deepEqual(actual, [10, 20, 30, 40, 50]);
+
+    assert.end();
+});
